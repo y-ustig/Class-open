826e4ffb14b3a19d0d88d6e677d541605f6b8482
y-ustig <yusting16@gmail.com>
2022-07-19 14:33:58 -0500

    first commit

---
+++ Open_boot/chapter4/task4.java
@@ -0,0 +1,23 @@
+import javax.lang.model.util.ElementScanner14;
+
+/**
+ * task4
+ */
+public class task4 {
+public static void main(String[] args) {
+int numeroif = 45;
+
+if (numeroif > 0){
+    System.out.println("el numero es positivo");
+    }
+else if (numeroif < 0){
+        System.out.println("el numero es negativo");
+    }
+else
+    {
+        System.out.println("EL VALOR ES CERO");
+    }
+}
+}
+    
+
