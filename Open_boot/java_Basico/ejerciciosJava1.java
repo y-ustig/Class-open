ae824f7f98007394f5f50394edb9493d6dfa34b7
y-ustig <yusting16@gmail.com>
2022-09-02 12:01:40 -0500

    aprendiendo java

---
+++ Open_boot/java_Basico/ejerciciosJava1.java
@@ -0,0 +1,32 @@
+public class ejerciciosJava1 {
+        public static void main(String[] args) {
+    
+            //datos numericos
+    
+            byte variable1 = 27;
+            short variable2 = 4;
+            int variable3 = 3125631;
+            long variable4 = 20;
+    
+            // datos decimales
+    
+    
+            float variable5 = 1.67f;
+            double variable6 = 1.200d;
+    
+            // datos booleanos
+    
+            boolean variable7 = true;
+            boolean variable8 = false;
+    
+            // datos de texto
+    
+            char variable9 = 'a';
+            String variable10 = "Esteban";
+    
+            System.out.print("me llamo " + variable10 + " tengo " + variable1 + " años, llevo estudiando " + variable2 + " meses ");
+            System.out.print(" mi numero de telefono es " + variable3 + " estoy practicando ciclismo, actualmente pedaleo a " + variable4 + "km");
+            System.out.println(" mido " + variable5 + " cm y estoy apuntando a ganar " + variable6 + " en USD" );
+    
+}
+}
