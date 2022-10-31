9db18df0d543f4cf89d2f83c17689eacec995a0d
y-ustig <yusting16@gmail.com>
2022-10-31 12:30:22 -0500

    ingresando CSS

---
+++ Clase_html/Form/MaquinaEscribir.js
@@ -0,0 +1,15 @@
+const Maquina1 = document.getElementsByClassName(Letras);
+
+let nombre = [ "Pymes" , "Propiedades Horizontales", "Start Ups" ]
+
+function devuelve(){
+
+for (var i = 0; i< nombre.length; i++){
+
+    let palabra = palabra + nombre[i];
+    return (palabra);
+}
+
+}
+
+devuelve ( Maquina1 );
